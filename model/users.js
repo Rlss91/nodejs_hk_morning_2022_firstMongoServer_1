2e978b4953a4005b49b14adb0fbe91579ab12615
rlss91 <rlss91@gmail.com>
2022-01-18 15:42:42 +0200

    mongoose crud

--- model/users.js
+++ model/users.js
@@ -5,6 +5,40 @@ const Schema = mongoose.Schema;
 const usersSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Users", usersSchema);
+const Users = mongoose.model("Users", usersSchema);
+
+const insertUser = (name, email, password) => {
+  const user = new Users({
+    name,
+    email,
+    password,
+  });
+  return user.save();
+};
+
+const updateUserById = (name, email, password) => {
+  Users.findById("61e6af387c1af7af12d66c50").then((userDoc) => {
+    userDoc.name = name;
+    userDoc.email = email;
+    userDoc.password = password;
+    userDoc.save();
+  });
+};
+
+const deleteUserById = (id) => {
+  return Users.deleteOne({ _id: id });
+};
+
+const selectAllUsers = () => {
+  return Users.find({});
+};
+
+module.exports = {
+  insertUser,
+  updateUserById,
+  deleteUserById,
+  selectAllUsers,
+};
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  const newUser = new UserModel({
-    email: "1@2.com",
-    password: "1234",
-  });
-  newUser.save();
+router.get("/", async function (req, res) {
+  // UserModel.insertUser("kenny", "kenny@gmail.com", "1234");
+  // UserModel.updateUserById("john", "john@gmail.com", "1234");
+  // UserModel.deleteUserById("61e6af387c1af7af12d66c50").then((result) => {
+  //   console.log("result", result);
+  // });
+  try {
+    let data = await UserModel.selectAllUsers();
+    console.log(data);
+  } catch (err) {}
   res.render("index", { title: "Express" });
 });
 
